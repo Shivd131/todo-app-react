586fc5fc264101f858127599f5e2c8aa8ed6cc4a
Shivd131 <shivdeshpande131@gmail.com>
2024-03-29 18:19:55 +0530

    feat: setup tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className='bg-green-400'>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
